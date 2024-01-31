5e43b3ba1037f3a3732fb036799a7d7780562c04
Can Duman <112070129+can827@users.noreply.github.com>
2024-01-31 13:49:25 +0000

    Sub Menue hinzugefühgt

--- MultiTool/Hauptmenue.cs
+++ MultiTool/Hauptmenue.cs
@@ -66,6 +66,7 @@ namespace MultiTool
                     case "m":
                         Console.Clear();
                         //Hier das Mathematikmenü aufrufen
+                        MAKlassenBibliothek.MAMenue.MASubMenue();
                         break;
 
                     case "p":
