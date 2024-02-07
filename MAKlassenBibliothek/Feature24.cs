e1638907243bed387b2619dd7279276ba701eae0
Can Duman <112070129+can827@users.noreply.github.com>
2024-02-07 11:33:16 +0000

    Feature 24 fertig

---
+++ MAKlassenBibliothek/Feature24.cs
@@ -0,0 +1,10 @@
+namespace MAKlassenBibliothek;
+
+internal class Feature24
+{
+    internal static void Feature_24()
+    {
+        Console.WriteLine("Test Feature 24"); 
+        //Diese Ausgabe hilft Ihnen zu erkennen ob der Aufruf funktioniert.
+    }
+}
