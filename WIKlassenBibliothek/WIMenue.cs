b62e8843bb681a5c981777b0e137ebf164e15a59
Muhammed <141837723+MuhammedTR38@users.noreply.github.com>
2024-02-21 07:46:22 +0000

    update von Liste

--- WIKlassenBibliothek/WIMenue.cs
+++ WIKlassenBibliothek/WIMenue.cs
@@ -4,90 +4,91 @@ public class WIMenue
 {
     public static void WISubMenue()
     {
- bool Exit = false;
+        bool Exit = false;
 
-            do
+        do
+        {
+
+            (int, int) cPosBM = Console.GetCursorPosition();
+
+            Console.WriteLine();
+
+            //ASCII art Logo wird erzeugt.
+            Console.WriteLine
+                (FiggleFonts.Slant.Render("Wirtschaft"));
+
+            //Konsolentitel wird geändert.
+            Console.Title = "BFTMultiTool";
+
+            Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                "                              >>> Hauptmenü <<<\n" +
+                                "------------------------------------------------------------------------------------\n\n");
+
+            Console.WriteLine("Eingabe: exit\t->\tbeendet das Programm");
+
+            //Beschreibung der Software.
+            Console.WriteLine("\n\nMit der Software 'BFTMulti-Tool' sollen wiederkehrende oder besonders aufwendige\n" +
+                                "Aufgabenaus dem schulischen Kontext erleichtert oder gelöst werden. Diese Aufgaben\n" +
+                                "ergeben sich aus den Problemstellungen aus dem Unterricht der Berufsfachschule für Technik.\n\n");
+
+            string HauptAusw;
+
+
+            //Eingabeaufforderung 
+            Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
+            Console.WriteLine("\t1 - Elektrotechnik");
+            Console.WriteLine("\t2 - Zinsrechner");
+            Console.WriteLine("\t3 - Mathematik");
+            Console.WriteLine("\t4 - Physik");
+            Console.WriteLine("\t5 - Feature20(Namen hier ändern) \n");
+            Console.Write("Eingabe:");
+            HauptAusw = Console.ReadLine().ToLower();
+
+            switch (HauptAusw)
             {
+                case "1":
+                    Console.Clear();
+                    //Hier das Etechnikmenü aufrufen
+                    break;
 
-                (int, int) cPosBM = Console.GetCursorPosition();
+                case "2":
+                    Console.Clear();
+                    Feature17.Feature_17();
+                    //Hier das Informationstechnikmenü aufrufen
+                    break;
 
-                Console.WriteLine();
+                case "3":
+                    Console.Clear();
+                    //Hier das Mathematikmenü aufrufenc
+                    break;
 
-                //ASCII art Logo wird erzeugt.
-                Console.WriteLine
-                    (FiggleFonts.Slant.Render("Wirtschaft"));
+                case "4":
+                    Console.Clear();
+                    //Hier das Physikmenü aufrufen
+                    break;
 
-                //Konsolentitel wird geändert.
-                Console.Title = "BFTMultiTool";
+                case "5":
+                    Console.Clear();
+                    //Hier das Wirtschaftsmenü aufrufen
+                    //Aufruf Feature20
+                    break;
 
-                Console.WriteLine("------------------------------------------------------------------------------------\n" +
-                                  "                              >>> Hauptmenü <<<\n" +
-                                  "------------------------------------------------------------------------------------\n\n");
+                case "exit":
+                    Exit = true;
+                    break;
+                default:
 
-                Console.WriteLine("Eingabe: exit\t->\tbeendet das Programm");
+                    Console.WriteLine("Ungültige Eingabe");
+                    Console.ReadKey();
 
-                //Beschreibung der Software.
-                Console.WriteLine("\n\nMit der Software 'BFTMulti-Tool' sollen wiederkehrende oder besonders aufwendige\n" +
-                                  "Aufgabenaus dem schulischen Kontext erleichtert oder gelöst werden. Diese Aufgaben\n" +
-                                  "ergeben sich aus den Problemstellungen aus dem Unterricht der Berufsfachschule für Technik.\n\n");
+                    (int, int) cPosAM = Console.GetCursorPosition();
 
-                string HauptAusw;
+                    KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
 
+                    break;
+            }
 
-                //Eingabeaufforderung 
-                Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
-                Console.WriteLine("\t1 - Elektrotechnik");
-                Console.WriteLine("\t2 - Informatik");
-                Console.WriteLine("\t3 - Mathematik");
-                Console.WriteLine("\t4 - Physik");
-                Console.WriteLine("\t5 - Feature20(Namen hier ändern) \n");
-                Console.Write("Eingabe:");
-                HauptAusw = Console.ReadLine().ToLower();
-
-                switch (HauptAusw)
-                {
-                    case "1":
-                        Console.Clear();
-                        //Hier das Etechnikmenü aufrufen
-                        break;
-
-                    case "2":
-                        Console.Clear();
-                        //Hier das Informationstechnikmenü aufrufen
-                        break;
-
-                    case "3":
-                        Console.Clear();
-                        //Hier das Mathematikmenü aufrufenc
-                        break;
-
-                    case "4":
-                        Console.Clear();
-                        //Hier das Physikmenü aufrufen
-                        break;
-
-                    case "5":
-                        Console.Clear();
-                        //Hier das Wirtschaftsmenü aufrufen
-                        //Aufruf Feature20
-                        break;
-
-                    case "exit":
-                        Exit = true;
-                        break;
-                    default:
-
-                        Console.WriteLine("Ungültige Eingabe");
-                        Console.ReadKey();
-
-                        (int, int) cPosAM = Console.GetCursorPosition();
-
-                        KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
-
-                        break;
-                }
-
-            } while (!Exit);
+        } while (!Exit);
 
     }
 }
